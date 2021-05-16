a54f91ede252b218f977ff0c2ca191b00269eb80
Squid Tseng <47516926+james604s@users.noreply.github.com>
2021-05-16 11:21:41 +0800

    db migrate update

--- basic.py
+++ basic.py
@@ -1,6 +1,7 @@
 import os 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate #pip install Flask-Migrate
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 #/Users/sq/flask_proj/flask-blog
@@ -11,7 +12,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASEDIR, "da
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-
+#flask db migrate -m "some message"
+Migrate(app, db)
 #####################################
 class Puppy(db.Model):
     #MANUAL TABLE NAME CHOICE
@@ -19,11 +21,51 @@ class Puppy(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
-    age = db.Column(db.Integer)
+    """One:One To Many, Puppy to Many Toys"""
+    toys = db.relationship("Toy", backref="puppy", lazy='dynamic')
+    """One to One, One Puppy to One Owner"""
+    owner = db.relationship("Toy", backref="puppy", uselist=False)
 
-    def __init__(self, name, age):
+    def __init__(self, name, toys):
         self.name = name
-        self.age = age
 
-    def __repr__(self):
-        return f"Puppy {self.name} is {self.age} years old"
+    def __repr(self):
+        if self.owner:
+            return f"Puppy name is {self.name} and \
+                    owner is {self.owner.name}"
+        else:
+            return f"Puppy name is {self.name} and \
+                    and has no owner yet!"
+
+    def report_toys(self):
+        print("Here are my toys:")
+        for toy in self.toys:
+            print(toy.item_name)
+class Toy(db.Model):
+    __tablename__ = "toys"
+    
+    id = db.Column(db.Integer, primary_key = True)
+    item_name = db.Column(db.Text)
+    puppy_id = db.Column(db.Interger, db.Foreign("Puppies.id"))
+    
+    def __init__(self, item_name, puppy_id):
+        self.item_name = item_name
+        self.puppy_id = puppy_id
+
+class Owner(db.Model):
+    __tablename__ = "Owners"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.Text)
+    puppy_id = db.Column(db.Interger, db.Foreign("Puppies.id"))
+
+    def __init__(self, name, puppy_id):
+        self.name = name
+        self.puppy_id = puppy_id
+
+    # def __init__(self, name, age, breed):
+    #     self.name = name
+    #     self.age = age
+    #     self.breed = breed
+
+    # def __repr__(self):
+    #     return f"Puppy {self.name} is {self.age} years old"
