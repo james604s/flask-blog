538fca65b96d1dca13b94b3a3a18a785b47a935a
Squid Tseng <47516926+james604s@users.noreply.github.com>
2021-05-18 23:49:41 +0800

    update Flask-Login3

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -2,11 +2,15 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
+
+# Create a login manager object
+login_manager = LoginManager()
 
 app = Flask(__name__)
 
-# Often people will also separate these into a separate config.py file 
+# Often people will also separate these into a separate config.py file
 app.config['SECRET_KEY'] = 'mysecretkey'
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
@@ -15,11 +19,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app,db)
 
+# We can now pass in our app to the login manager
+login_manager.init_app(app)
+
+# Tell users what view to go to when they need to login.
+login_manager.login_view = "login"
+
 # NOTE! These imports need to come after you've defined db, otherwise you will
 # get errors in your models.py files.
 ## Grab the blueprints from the other views.py files for each "app"
-from myproject.puppies.views import puppies_blueprint
-from myproject.owners.views import owners_blueprint
+# from myproject.puppies.views import puppies_blueprint
+# from myproject.owners.views import owners_blueprint
 
-app.register_blueprint(owners_blueprint,url_prefix="/owners")
-app.register_blueprint(puppies_blueprint,url_prefix='/puppies')
+# app.register_blueprint(owners_blueprint,url_prefix="/owners")
+# app.register_blueprint(puppies_blueprint,url_prefix='/puppies')
